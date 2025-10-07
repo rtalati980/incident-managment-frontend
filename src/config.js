9a1bc420a05efdb888cc93ed7f5871e3a0ff7941
RONAK TALATI <96633606+rtalati980@users.noreply.github.com>
2025-10-07 12:45:55 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 // config.js
 const config = {
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000' || 'https://incident-managment-backend.onrender.com',
+    API_BASE_URL: 'https://incident-managment-backend.onrender.com' || 'http://localhost:5000' || 'https://incident-managment-backend.onrender.com',
   };
   
   export default config;
