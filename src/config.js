8da6af8dab7713cc6709acdead344ba2796cd83b
RONAK TALATI <96633606+rtalati980@users.noreply.github.com>
2025-10-28 22:32:32 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 // config.js
 const config = {
-    API_BASE_URL: 'http://localhost:5000' || 'https://incident-managment-backend.onrender.com',
+    API_BASE_URL: 'http://localhost:5000' || 'https://incident-managment-backend.onrender.com'
   };
   
   export default config;
